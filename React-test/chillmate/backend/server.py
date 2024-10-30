12cfe0d0e88750e252cb3198523652d7401574d7
willi301 <will301will@gmail.comm>
2024-10-30 15:15:17 -0700

    created forum api, no functionality

--- React-test/chillmate/backend/server.py
+++ React-test/chillmate/backend/server.py
@@ -17,6 +17,9 @@ client = MongoClient(mongo_uri)
 db = client['your_database_name']
 users_collection = db['users']
 
+db2 = client['chillmate']
+forum_collection = db2['Forum']
+
 # Route for seeing a data
 @app.route('/data')
 def get_time():
@@ -59,6 +62,28 @@ def login():
             # Add any additional logic, such as session management
     else:
             return jsonify({"message": "error"}), 202
+
+#api to create new post
+@app.route('/createPost', method=('GET','POST'))
+def createPost():
+    data = request.json
+    Topic = data.get("topic")
+    Text = data.get("text")
+    NoofLikes = 0
+
+    new_post = {
+            "Topic": Topic,
+            "Text": Text,  # Ideally, hash the password before storing
+            "NoofLikes": NoofLikes
+    }
+
+    forum_collection.insert_one(new_post)
+    return jsonify({"message": "User registered successfully"}), 201
+
+@app.route('/getPost', method=('GET', 'POST'))
+def getPost():
+    post = forum_collection.query.all()
+    return jsonify([{'Topic': post.Topic, 'Text': post.Text, 'NoofLikes': post.NoofLikes} for post in post])
 
     
 # Running app
