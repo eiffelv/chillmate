fd860bae2bad2e0c20e986ff41554547b777333e
willi301 <will301will@gmail.comm>
2024-11-10 13:41:57 -0800

    updated alert

--- app/src/components/register.js
+++ app/src/components/register.js
@@ -54,6 +54,7 @@ const Register = () => {
             const data = await response.json();
 
             if (response.ok) {
+                alert("registered successfully!");
                 setMessage(data.message);
             } else {
                 setMessage(data.error || "An error occurred");
