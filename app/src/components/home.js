f0df4f52709b7ef1e20503ed15d6db524307e667
Myst1cLeaf <lcarrillo2@sfsu.edu>
2024-11-12 17:55:20 -0800

    Modifying CSS for home page to make it more appealing

--- app/src/components/home.js
+++ app/src/components/home.js
@@ -4,6 +4,11 @@ import { LoginContext } from "./LoginContext";
 import PropTypes from "prop-types";
 import "./style.css";
 import "./ChillMateLogo.png";
+import Chatbot from "./Chatbot.png";
+import Forum from "./Forum.png";
+import Journal from "./Journal.png";
+import Resources from "./Resources.png";
+import Tasklist from "./TaskList.png";
 
 
 function Home() {
@@ -16,23 +21,45 @@ function Home() {
     };
 
     return (
-        <div>
-            <div className="container" id="home">
-                <h1>ChillMate</h1>
-                <div className="homeDescription">
-                    <p>Our stress management app is designed for San Francisco State University students and faculty, offering essential tools to combat stress:</p>
-                    <ul className="a">
-                        <li>Chatbot Support: Get 24/7 personalized assistance and coping strategies.</li>
-                        <li>Journal: Track your thoughts and emotions to enhance well-being.</li>
-                        <li>Forum: Connect with peers for support and advice.</li>
-                        <li>Resources: Access a library of articles and techniques on mindfulness and relaxation.</li>
-                        <li>Task List: Stay organized and manage your responsibilities effectively.</li>
-                    </ul>
-                    <p>Join us in fostering a healthier academic environment!</p>
-                </div>
-                <button onClick={goToRegister}>Register Now!</button>
+        <div className="containerHome" id="home">
+            <h1>ChillMate</h1>
+            <div className="missionStatement">
+                 <h2>Our Mission</h2>
+                <h3>
+                    We <em>empower</em> you with the knowledge and skills you need to strengthen                        your mental health & well-being
+                </h3>
             </div>
+            <div className="features">
+                <div className="feature">
+                    <h4>Chatbot Support</h4>
+                    <p>Get 24/7 personalized assistance and coping strategies.</p>
+                    <img src={Chatbot} width="100" height="100" alt="Chatbot" />
+                </div>
+                <div className="feature">
+                    <h4>Journal</h4>
+                    <p>Track your thoughts and emotions to enhance well-being.</p>
+                    <img src={Journal} width="100" height="100" alt="Journal" />
+                </div>
+                <div className="feature">
+                    <h4>Forum</h4>
+                    <p>Connect with peers for support and advice.</p>
+                    <img src={Forum} width="100" height="100" alt="Forum" />
+                </div>
+                <div className="feature">
+                    <h4>Resources</h4>
+                    <p>Access a library of articles and techniques on mindfulness and relaxation.</p>
+                    <img src={Resources} width="100" height="100" alt="Resources" />
+                </div>
+                <div className="feature">
+                    <h4>Task List</h4>
+                    <p>Stay organized and manage your responsibilities effectively.</p>
+                    <img src={Tasklist} width="100" height="100" alt="TaskList" />
+                </div>
+            </div>
+            <p>Join us in fostering a healthier academic environment!</p>
+            <button onClick={goToRegister}>Register Now!</button>
         </div>
+        
     );
 }
 
